98a3229e3c79ae8e1731166e95c789bdd18153f5
PraneethGunas <praneethgunas404@gmail.com>
2020-06-13 02:39:31 +0530

    firebase setup

---
+++ music-app/src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: "bitify-7d21a.firebaseapp.com",
+  databaseURL: "https://bitify-7d21a.firebaseio.com",
+  projectId: "bitify-7d21a",
+  storageBucket: "bitify-7d21a.appspot.com",
+  messagingSenderId: "65565251263",
+  appId: "1:65565251263:web:f5f52d19a4954d0b31180a",
+  measurementId: "G-VTX13ZFZRH",
+};
+
+firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
