02f2a1eead1e2192b2a32db52401205b1401b3ac
Pratyaksh Chandra <pratyaksh.chandra@hashedin.com>
2020-06-13 15:12:25 +0530

    yMerge branch 'master' of personal:PraneethGunas/Bitify

--- music-app/src/App.js
+++ music-app/src/App.js
@@ -4,37 +4,48 @@ import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
 import "./App.css";
+import HelloWorld from "./artifacts/HelloWorld.json";
+import Migrations from "./artifacts/Migrations.json";
+import { Drizzle } from "@drizzle/store";
+import { DrizzleContext } from "@drizzle/react-plugin";
+const drizzleOptions = {
+  contracts: [HelloWorld, Migrations],
+};
+
+const drizzle = new Drizzle(drizzleOptions);
 
 export default function App() {
   return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/signup">Signup</Link>
-          </li>
-        </ul>
+    <DrizzleContext.Provider drizzle={drizzle}>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </ul>
 
-        <hr />
+          <hr />
 
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/signup">
-            <Signup />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </DrizzleContext.Provider>
   );
 }
--- music-app/src/Pages/Login.js
+++ music-app/src/Pages/Login.js
@@ -1,8 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { firestore } from "../firebase";
+import { useHistory } from "react-router-dom";
+
+const db = firestore;
 const Login = () => {
+  const [walletID, setWalletID] = useState("");
+  const [username, setUsername] = useState("");
+  const [error, setError] = useState(null);
+  const history = useHistory();
+  const signInWithwalletIDAndusernameHandler = async (
+    event,
+    walletID,
+    username
+  ) => {
+    try {
+      event.preventDefault();
+      const docRef = await db.collection("users").doc(walletID);
+      const user = await docRef.get();
+      if (user.exists) {
+        alert("User Exists");
+        history.push("/home");
+      } else {
+        alert("No user");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onChangeHandler = (event) => {
+    const { name, value } = event.currentTarget;
+
+    if (name === "walletid") {
+      setWalletID(value);
+    } else if (name === "username") {
+      setUsername(value);
+    }
+  };
+
   return (
     <div>
-      <h2>Login</h2>
+      <h1>Sign In</h1>
+      <div>
+        {error !== null && <div>{error}</div>}
+        <form>
+          <label htmlFor="walletid" className="block">
+            Wallet ID
+          </label>
+          <input
+            type="text"
+            name="walletid"
+            value={walletID}
+            placeholder="Wallet ID"
+            id="walletid"
+            onChange={(event) => onChangeHandler(event)}
+          />
+          <label htmlFor="username" className="block">
+            Name
+          </label>
+          <input
+            type="text"
+            name="username"
+            value={username}
+            placeholder="Your usename"
+            id="username"
+            onChange={(event) => onChangeHandler(event)}
+          />
+          <button
+            onClick={(event) => {
+              signInWithwalletIDAndusernameHandler(event, walletID, username);
+            }}
+          >
+            Sign in
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
