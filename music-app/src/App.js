35d9c2843e36d93614a0ec4776c93b0cd8fafff4
Pratyaksh Chandra <pratyaksh.chandra@hashedin.com>
2020-06-14 02:29:13 +0530

    update

--- music-app/src/App.js
+++ music-app/src/App.js
@@ -1,13 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Home from "./Pages/Home";
-import Login from "./Pages/Login";
-import Signup from "./Pages/Signup";
-import "./App.css";
-=======
 import Routes from "./Pages/Route";
->>>>>>> ed782e11265141508c061576d79184d29eee61ef
 import HelloWorld from "./artifacts/HelloWorld.json";
 import Migrations from "./artifacts/Migrations.json";
 import { Drizzle } from "@drizzle/store";
